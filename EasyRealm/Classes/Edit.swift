cac68f7b15c5947fd101d48ae7f17e16be6cd059
Dmitry Stepanets <stepanets90@gmail.com>
2018-06-06 16:27:02 +0500

    Add ignored notification tokens to edt method

--- EasyRealm/Classes/Edit.swift
+++ EasyRealm/Classes/Edit.swift
@@ -11,22 +11,27 @@ import Realm
 import RealmSwift
 
 extension EasyRealm where T:Object {
-  
-  public func edit(_ closure: @escaping (_ T:T) -> Void) throws {
-    self.isManaged ? try managed_edit(closure) : try unmanaged_dit(closure)
-  }
-  
+    public func edit(withoutNotifying:[NotificationToken]? = nil, _ closure: @escaping (_ T:T) -> Void) throws {
+        self.isManaged ? try managed_edit(withoutNotifying: withoutNotifying, closure) : try unmanaged_dit(closure)
+    }
 }
-
 
 fileprivate extension EasyRealm where T:Object {
 
-  fileprivate func managed_edit(_ closure: @escaping (_ T:T) -> Void) throws {
+  fileprivate func managed_edit(withoutNotifying:[NotificationToken]?, _ closure: @escaping (_ T:T) -> Void) throws {
     guard let rq = EasyRealmQueue() else { throw EasyRealmError.RealmQueueCantBeCreate }
     let ref = ThreadSafeReference(to: self.base)
     try rq.queue.sync {
       guard let object = rq.realm.resolve(ref) else { throw EasyRealmError.ObjectCantBeResolved }
-      try rq.realm.write { closure(object) }
+        
+        guard let notifications = withoutNotifying else {
+            try rq.realm.write { closure(object) }
+            return
+        }
+        
+        rq.realm.beginWrite()
+        closure(object)
+        try rq.realm.commitWrite(withoutNotifying: notifications)
     }
   }
   
